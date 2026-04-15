a76bdcc71f1153666652774e0759808c0d7b2307
divanethcordoba407-png <divanethcordoba407@gmail.com>
2026-04-15 17:26:25 -0500

    Add files via upload

---
+++ Primo/src/primo/Primo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primo;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Primo {
+
+    public static boolean esPrimo(int num) {
+
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
